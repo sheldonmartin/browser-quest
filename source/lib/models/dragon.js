5e4ca5545098d81769bb1f0ad4d3eadcea520780
Sheldon Martin <sheld0ri@gmail.com>
2015-08-28 16:59:19 -0400

    Implements Dragon.attack

--- source/lib/models/dragon.js
+++ source/lib/models/dragon.js
@@ -1,5 +1,24 @@
 var Dragon = function() {
+  var damage = 50;
+  var vitality = 5000;
 
+  function attack(target) {
+    target.takeDamage(damage);
+  }
+
+  function takeDamage(damage) {
+    vitality -= damage;
+  }
+
+  function health() {
+    return vitality;
+  }
+
+  return {
+    attack: attack,
+    takeDamage: takeDamage,
+    health: health
+  }
 }
 
 module.exports = Dragon;
