3ce0eb3db38732f008dc0bfb69ec51e5d0820ae1
Sheldon Martin <sheld0ri@gmail.com>
2015-08-28 11:48:04 -0400

    Adds spec for Dragon.attack

--- source/spec/models/dragon_spec.js
+++ source/spec/models/dragon_spec.js
@@ -1,7 +1,15 @@
 describe('Dragon', function() {
   var Dragon = require('../../lib/models/dragon.js');
+  var Enemy = require('../../lib/models/dragon.js');
 
   beforeEach(function() {
     var dragon = Dragon();
+    var enemy = Enemy();
+  });
+
+  it("should be able to attack and cause damage to enemies", function() {
+    dragon.attack(enemy);
+
+    expect(dragon.health()).toEqual(4950);
   });
 });
